203287eb85187d5938431117d58f3ff1ad66a5ec
Lavell Wesley <lavellthedev+github@gmail.com>
2020-04-10 01:06:04 -0400

    forget what push is

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,1 +1,32 @@
-//Have to update this thing
+/*Get Our Elemens*/
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/*Build out functions*/
+function togglePlay(){
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton(){
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+
+function skip() {
+  console.log('skipping')
+}
+
+/* Hook up the event listners*/
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click',.add togglePlay);
+skipButtons.forEach(button => button.addEventListener)
