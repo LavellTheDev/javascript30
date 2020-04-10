a4a90d959242f24833aaf5933b8a6916ecc555a4
Lavell Wesley <lavellthedev+github@gmail.com>
2020-04-10 01:53:23 -0400

    working on progress bar

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -20,13 +20,29 @@ function updateButton(){
 
 
 function skip() {
-  console.log('skipping')
+  console.log(this.name);
+  console.log('skipping');
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+  video[this.name] = this.value;
+  console.log(this.value);
+}
+
+function handleProgess(){
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
 }
 
 /* Hook up the event listners*/
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgess);
 
-toggle.addEventListener('click',.add togglePlay);
-skipButtons.forEach(button => button.addEventListener)
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button  => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
